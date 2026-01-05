1e5a07f5951712bdcd3374a81f78863dc62d330d
VeroDominguez <153690222+VeroDominguez@users.noreply.github.com>
2026-01-05 01:41:24 -0300

    se genera estructura base del Mock falta: -cambair url para engancharla con el back -agrega que envie 3 placas internas a la vez -ampliar los casos de uso

---
+++ MockClasses/DTOs/DataArduinoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockClasses.DTOs
+{
+    public class DataArduinoDto
+    {
+        public float TempInterna1 { get; set; }
+        public float TempInterna2 { get; set; }
+        public float TempInterna3 { get; set; }
+        public float TempExterna { get; set; }
+        public float Peso { get; set; }
+    }
+}
---
+++ MockConsola/Servicios/EnvioDatosService.cs
@@ -0,0 +1,31 @@
+﻿using MockClasses.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MockConsola.Servicios
+{
+    public class EnvioDatosService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EnvioDatosService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task EnviarAsync(DataArduinoDto dto)
+        {
+            var json = JsonSerializer.Serialize(dto);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            await _httpClient.PostAsync(
+                "https://localhost:5001/api/registros", //aca va la url del endpoint para recibir los datos,      CAMBIAR antes de usar
+                content
+            );
+        }
+    }
+}
